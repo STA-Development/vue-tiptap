cff4057854db8dcf768718f507f4c58fc146d6ff
Arthur02V <artur.voskanyan@stayopn.com>
2023-06-23 01:50:25 +0400

    FEAT: add dragable div elements

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,60 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div v-if="editor">
+    <editor-content :editor="editor" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import StarterKit from "@tiptap/starter-kit";
+import { Editor, EditorContent } from "@tiptap/vue-3";
+
+import DraggableItem from "./components/Extensions/DraggableItem.ts";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    EditorContent,
+  },
+
+  data() {
+    return {
+      editor: null,
+    };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      extensions: [StarterKit, DraggableItem],
+      content: `
+        <p>This is a boring paragraph.</p>
+        <div data-type="draggable-item">
+          <p>Followed by a fancy draggable item.</p>
+        </div>
+        <div data-type="draggable-item">
+          <p>And another draggable item.</p>
+          <div data-type="draggable-item">
+            <p>And a nested one.</p>
+            <div data-type="draggable-item">
+              <p>But can we go deeper?</p>
+            </div>
+          </div>
+        </div>
+        <p>Let’s finish with a boring paragraph.</p>
+        <div data-type="draggable-item">
+          <ul data-type="draggable-item">
+
+            <li class="drag-handle" data-type="draggable-item" data-drag-handle>Something2</li>
+
+            <li class="drag-handle" data-type="draggable-item" data-drag-handle>Something3</li>
+
+            <li class="drag-handle" data-type="draggable-item" data-drag-handle>Something4</li>
+          </ul>
+        </div>
+      `,
+    });
+  },
+
+  beforeUnmount() {
+    this.editor.destroy();
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
